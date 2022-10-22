ce7e4fd24415a53fab81be9c69d9135b36888b95
Iliyan <boneff.i@gmail.com>
2022-10-22 11:17:41 +0300

    Refactor scraper, break tests

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from dotenv import load_dotenv
 import logging
 import os
 import requests
-
-relative_url = "/web/20200207180731/https://en.wikipedia.org/wiki/Web_scraping"
-base_url = "https://web.archive.org"
+from tld import get_tld, get_fld
 
 def setup():
     load_dotenv()
@@ -17,9 +15,7 @@ def setup():
 
     logging.basicConfig(filename='./logs/crawler-{}-{}.log'.format(datetime_string, env), encoding='utf-8', level=logging.DEBUG)
 
-def scrape_url(base_url, relative_url):
-    absolute_url = base_url + relative_url
-
+def scrape_url(absolute_url):
     logging.info('Starting web crawler')
     logging.debug('Fetching data from: {}'.format(absolute_url))
     try:
@@ -30,15 +26,15 @@ def scrape_url(base_url, relative_url):
     except requests.exceptions.ConnectionError:
         logging.error('Connection error.')
         return ''
+    logging.debug('Fetched data {} from: {}'.format(req.text, absolute_url))
 
     return req.text
 
-def parse_html(html):
+def parse_html(html, target_element, target_class = ''):
     link_dict = {}
 
     soup = BeautifulSoup(html, 'html.parser')
-    target_element = 'div'
-    target_class = 'div-col columns column-width'
+
     link_div = soup.find(target_element, {'class': target_class})
     if link_div is None:
         logging.error("Target element '{}' with class '{}' not found".format(target_element, target_class))
@@ -46,15 +42,56 @@ def parse_html(html):
 
     # Passing a list to find_all method
     for element in link_div.find_all('a'):
-        link_dict[element.get_text()] = base_url + element['href']
+        link_dict[element.get_text()] = element['href']
 
     return link_dict
 
+
+def get_normalized_url(url):
+    res = get_tld(url, as_object=True)
+    path_list = [char for char in res.parsed_url.path]
+    if len(path_list) == 0:
+        final_url = res.parsed_url.scheme+'://'+res.parsed_url.netloc
+    elif path_list[-1] == '/':
+        final_string = ''.join(path_list[:-1])
+        final_url = res.parsed_url.scheme+'://'+res.parsed_url.netloc+final_string
+    else:
+        final_url = url
+    return final_url
+
+def complete_crawler(seed_url, max_n = 1):
+    delay_time = 0.5
+    target_element = 'div'
+    target_class = ''
+    initial_url_set = set()
+    initial_url_list = []
+    seen_url_set = set()
+    base_url = 'http://www.' + get_fld(seed_url)
+    res = get_tld(seed_url, as_object=True)
+    domain_name = res.fld
+    initial_url_set.add(seed_url)
+    initial_url_list.append(seed_url)
+    link_dictionaries = []
+    while len(initial_url_set) != 0 and len(seen_url_set) < max_n:
+        temp_url = initial_url_set.pop()
+        if temp_url in seen_url_set:
+            continue
+        else:
+            seen_url_set.add(temp_url)
+            req_text = scrape_url(temp_url)
+            links_dictionary = parse_html(req_text, target_element, target_class)
+            link_dictionaries.append(links_dictionary)
+            # print(links_dictionary)
+    return link_dictionaries
+
 if __name__ == '__main__':
     setup()
-    req_text = scrape_url(base_url, relative_url)
-    links_dictionary = parse_html(req_text)
-    print(links_dictionary)
-    for key, value in links_dictionary.items():
-        if "scraping" in key:
-            print(value)
+    # req_text = scrape_url(base_url, relative_url)
+    # links_dictionary = parse_html(req_text)
+    # print(links_dictionary)
+    # for key, value in links_dictionary.items():
+    #     if "scraping" in key:
+    #         print(value)
+
+    scraped_links = complete_crawler("http://www.dnes.bg/", 1)
+    print(scraped_links)
